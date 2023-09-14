492914d81d5702e1b2bac3a3ffa90e0860bdcb1f
Davi Nakano <davinakanoca@gmail.com>
2023-09-14 16:48:18 -0700

    chore: add and configure styled-components

---
+++ src/styles/global.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${(props) => props.theme.colors.blue};
+  }
+
+  body {
+    background: ${(props) => props.theme.colors.baseBackground};
+    color: ${(props) => props.theme.colors.baseText};
+  }
+
+  body, input, textarea, button {
+    font-family: ${(props) => props.theme.typography.fontFamily};
+    font-weight: 400;
+    font-size: 1rem;
+    line-height: 1.6;
+  }
+`;
